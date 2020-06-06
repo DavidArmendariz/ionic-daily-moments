6a8d11f014801ea5b47524d40e7f87cb681a60a5
David Armendariz <darmendariz1998@outlook.com>
2020-06-05 23:31:13 -0500

    added login flow

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,35 @@ import {
   IonLabel,
   IonIcon,
 } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 import HomePage from './pages/HomePage';
 import { Route, Redirect } from 'react-router-dom';
 import SettingsPage from './pages/Settings';
 import { IonReactRouter } from '@ionic/react-router';
 import { home as homeIcon, settings as settingsIcon } from 'ionicons/icons';
 import EntryPage from './pages/EntryPage';
+import LoginPage from './pages/LoginPage';
 
 const App: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  console.log(`Rendeering App with loggedIn=${loggedIn}`);
   return (
     <IonApp>
       <IonReactRouter>
         <IonTabs>
           <IonRouterOutlet>
-            <Route exact path="/entries" component={HomePage} />
+            <Route exact path="/entries">
+              {loggedIn ? <HomePage /> : <Redirect to="/login" />}
+            </Route>
             <Route exact path="/settings" component={SettingsPage} />
             <Redirect exact path="/" to="/entries" />
             <Route path="/entries/:id" component={EntryPage} />
+            <Route exact path="/login">
+              <LoginPage
+                onLogin={() => setLoggedIn(true)}
+                loggedIn={loggedIn}
+              />
+            </Route>
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
             <IonTabButton tab="Home" href="/entries">
