041d3836f7d48144f0ec8d8dd56be2337475c11d
David Armendariz <darmendariz1998@outlook.com>
2020-06-15 09:10:17 -0500

    enabling cloud storage

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDn681kurHhJuPsR_nauoKbODKjWDeA5D8',
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = app.auth();
 export const firestore = app.firestore();
+export const storage = app.storage();
