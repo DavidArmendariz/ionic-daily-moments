317494ce9b63377558cab14edbb0935387340d7a
David Armendáriz <darmendariz1998@outlook.com>
2020-06-16 22:18:36 -0500

    deleted api key

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDn681kurHhJuPsR_nauoKbODKjWDeA5D8',
-  authDomain: 'daily-moments-d3e5d.firebaseapp.com',
-  databaseURL: 'https://daily-moments-d3e5d.firebaseio.com',
-  projectId: 'daily-moments-d3e5d',
-  storageBucket: 'daily-moments-d3e5d.appspot.com',
-  messagingSenderId: '630456930591',
-  appId: '1:630456930591:web:a0d39c5ee482342d0e202a',
+  // your firebase config
 };
 
 const app = firebase.initializeApp(firebaseConfig);
